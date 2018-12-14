730afcb1a15adc551a709b550e7c50789389b5ce
Padam Bhandari <Padam.Bhandari@datascan.com>
2018-12-14 16:23:03 -0600

    Passing Method References between components

--- src/App.js
+++ src/App.js
@@ -26,12 +26,24 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-      <h1>I am react App</h1>
+      <h1>React Demo App</h1>
       <p> This is a p tag </p>
       <button onClick={this.swithNameHandler}> Switch Name </button>
-      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} ></Person>      
-      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} ></Person>      
-      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} ></Person>      
+      <Person
+       name = {this.state.persons[0].name}
+        age = {this.state.persons[0].age} >
+      </Person> 
+
+      <Person 
+      name = {this.state.persons[1].name}
+       age = {this.state.persons[1].age} 
+       click = {this.swithNameHandler}>
+       </Person>    
+
+      <Person 
+      name = {this.state.persons[2].name}
+      age = {this.state.persons[2].age} >
+      </Person>      
       </div>
     );
   }
