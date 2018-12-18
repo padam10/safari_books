7a6d987152987a88e24dd43dc5577238eddcff48
Padam Bhandari <Padam.Bhandari@datascan.com>
2018-12-18 10:33:20 -0600

    Outputing List

--- src/App.js
+++ src/App.js
@@ -56,22 +56,10 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div >
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} >
-          </Person>
-
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.swithNameHandler.bind(this, "Kramer")}
-            changed={this.nameChangedHandler}> My Hobbies: racing
-        </Person>
-
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} >
-          </Person>
+          {this.state.persons.map(person =>{
+            return <Person name = {person.name}
+            age = {person.age}/>
+          })}          
         </div>
 
       );
