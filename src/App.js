f34ae69fbd695e0d41af89bee81148d6c11d89b1
Padam Bhandari <Padam.Bhandari@datascan.com>
2018-12-18 18:10:43 -0600

    Added Flexible Lists

--- src/App.js
+++ src/App.js
@@ -16,14 +16,20 @@ class App extends Component {
   }
 
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "PadamB", age: "28" },
-        { name: event.target.value, age: "49" },
-        { name: "Elaine", age: "53" }
-      ]
-    })
+  nameChangedHandler = (event,id) => {
+    const personIndex = this.state.persons.findIndex(p =>{
+     return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons:persons });
   }
 
 deletePersonHandler = (personIndex) => {
@@ -58,7 +64,9 @@ deletePersonHandler = (personIndex) => {
             click = {()=> this.deletePersonHandler(index)}
               name={person.name}
               age={person.age} 
-              key = {person.id}/>
+              key = {person.id}
+              changed = {(event)=> this.nameChangedHandler(event, person.id)}            
+              />
           })}
         </div>
 
